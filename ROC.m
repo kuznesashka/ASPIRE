d69a81dffa7ed9babaaa7d3d79b435a1a1490c2d
Valerii <vagechirkov@gmail.com>
2019-11-14 10:06:57 +0300

    Update ROC

--- ROC.m
+++ ROC.m
@@ -53,11 +53,13 @@ for spikes_detection = detection_type%1:3
             ICA_roc = roc_time(ICA_visual(ICA_visual(:,1)<600000,:), ICA_grad, ICA_mag);
             ICA_roc_T = cell2table(ICA_roc(2:end,:));
             ICA_roc_T.Properties.VariableNames = ICA_roc(1,:);
+            ICA_roc_T.Properties.VariableNames(1) = {'ICA'};
 
             % ROC in spatial domain
             ICA_roc_spatial = roc_spatial(ICA_visual_grad, ICA_visual_mag, ICA_grad, ICA_mag);
             ICA_roc_spatial_T = cell2table(ICA_roc_spatial(2:end,:));
             ICA_roc_spatial_T.Properties.VariableNames = ICA_roc_spatial(1,:);
+            ICA_roc_spatial_T.Properties.VariableNames(1) = {'ICA'};
 
             % ROC in anatomical labels domain
             [ICA_labels_results, ICA_roc_labels] = roc_labels(ICA_visual_mag, ICA_visual_grad, ICA_mag, ...
@@ -65,9 +67,11 @@ for spikes_detection = detection_type%1:3
 
             ICA_labels_results_T = cell2table(ICA_labels_results(2:end,:));
             ICA_labels_results_T.Properties.VariableNames = ICA_labels_results(1,:);
+            ICA_labels_results_T.Properties.VariableNames(1) = {'ICA'};
 
             ICA_roc_labels_T = cell2table(ICA_roc_labels(2:end,:));
             ICA_roc_labels_T.Properties.VariableNames = ICA_roc_labels(1,:);
+            ICA_roc_labels_T.Properties.VariableNames(1) = {'ICA'};
 
             % Save results in the Excel file
             writetable(ICA_roc_T,fname,'Sheet',xlsTAB,'Range','A6');
@@ -98,11 +102,13 @@ for spikes_detection = detection_type%1:3
             SPC_roc = roc_time(SPC_visual(SPC_visual(:,1)<600000,:), SPC_grad, SPC_mag);
             SPC_roc_T = cell2table(SPC_roc(2:end,:));
             SPC_roc_T.Properties.VariableNames = SPC_roc(1,:);
+            SPC_roc_T.Properties.VariableNames(1) = {'SPC'};
 
             % ROC in spatial domain
             SPC_roc_spatial = roc_spatial(SPC_visual_grad, SPC_visual_mag, SPC_grad, SPC_mag);
             SPC_roc_spatial_T = cell2table(SPC_roc_spatial(2:end,:));
             SPC_roc_spatial_T.Properties.VariableNames = SPC_roc_spatial(1,:);
+            SPC_roc_spatial_T.Properties.VariableNames(1) = {'SPC'};
 
             % ROC in anatomical labels domain
             [SPC_labels_results, SPC_roc_labels] = roc_labels(SPC_visual_mag, SPC_visual_grad, SPC_mag, ...
@@ -110,15 +116,18 @@ for spikes_detection = detection_type%1:3
 
             SPC_labels_results_T = cell2table(SPC_labels_results(2:end,:));
             SPC_labels_results_T.Properties.VariableNames = SPC_labels_results(1,:);
+            SPC_labels_results_T.Properties.VariableNames(1) = {'SPC'};
 
             SPC_roc_labels_T = cell2table(SPC_roc_labels(2:end,:));
             SPC_roc_labels_T.Properties.VariableNames = SPC_roc_labels(1,:);
-            
+            SPC_roc_labels_T.Properties.VariableNames(1) = {'SPC'};
+
             % Save results in the Excel file
             writetable(SPC_roc_T,fname,'Sheet',xlsTAB,'Range','A1');
             writetable(SPC_roc_spatial_T,fname,'Sheet',xlsTAB,'Range','A11');
             writetable(SPC_roc_labels_T,fname,'Sheet',xlsTAB,'Range','A21');
             writetable(SPC_labels_results_T,fname_labels,'Sheet',xlsTAB,'Range','A1');
+            
             % Only for Windows
             %xlswrite(fname,SPC_roc,xlsTAB,'A1');
             %xlswrite(fname,{'SPC'},xlsTAB,'A1');
