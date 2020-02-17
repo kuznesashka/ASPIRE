164b7402d5e273cf6478f74cfbb5db4af54cf654
Valerii <vagechirkov@gmail.com>
2020-02-17 22:36:55 +0300

    Bug

--- main_one_subject_run_from_python.m
+++ main_one_subject_run_from_python.m
@@ -138,7 +138,7 @@ if parameters.spikes_detection == 1 % for manual spikes
     cluster{1,1} = [IndMax ; spike_ind  ; ValMax ; 1:length(IndMax); zeros(size(spike_ind))];
 else
     % cluster creation space
-    %try
+    try
         cluster = clustering(spike_ind, ... 
                              G3, ...
                              parameters.clustering.N_MIN, ... 
@@ -156,12 +156,12 @@ else
             % refine clusters throwing away multiple detection of spikes, only for Spyking Circus
             cluster = spykingcircus_cleaner_aftecluster(cluster);
         end
-    %catch
-    %    if size(IndMax) ~= size(spike_ind)
-    %        spike_ind  = spike_ind';
-    %    end
-    %    cluster{1,1} = [IndMax ; spike_ind  ; ValMax ; 1:length(IndMax)];
-    %end
+    catch
+        if size(IndMax) ~= size(spike_ind)
+            spike_ind  = spike_ind';
+        end
+        cluster{1,1} = [IndMax(ind_m); spike_ind(ind_m); ValMax(ind_m)];
+    end
     
 end
 
