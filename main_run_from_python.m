05cc02fd9011dfed12e30dd81eb8eb306028d23b
Valerii <vagechirkov@gmail.com>
2020-04-08 22:27:12 +0300

    ASPIRE on manual spikes

--- main_run_from_python.m
+++ main_run_from_python.m
@@ -121,7 +121,7 @@ parameters.clustering.N_MIN    = 5;
 
 if parameters.spikes_detection == 1 %visual
     parameters.clustering.THR_DIST = 0.02;
-    parameters.clustering.N_MIN     = 10;
+    parameters.clustering.N_MIN     = 5;
 end
 
 cortex          = load(paths.cortex);
